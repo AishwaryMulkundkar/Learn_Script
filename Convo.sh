3f9b1c990e25ae9d09b92f1e3a15c1d7dd2035bf
aishwarya sriharsha <aishwaryamulkundkar98@gmail.com>
2026-03-04 19:20:00 +0530

    Convo between aish and harsh

--- Convo.sh
+++ Convo.sh
@@ -5,6 +5,6 @@ Name2=Meenu
 
 
 echo "$Name1:: Hello $Name2",
-echo "$Name2::Hi $Name1, How are you ?",
-echo "$Name1::I am good,How about you ?",
-echo "$Name2::I am good"
+echo "$Name2:: Hi $Name1, How are you ?",
+echo "$Name1:: I am good,How about you ?",
+echo "$Name2:: I am good"
